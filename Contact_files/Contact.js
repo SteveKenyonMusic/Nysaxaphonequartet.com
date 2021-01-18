f149c1a8c27004b9a456dd706ce0579311f06319
SteveKenyonMusic <60806285+SteveKenyonMusic@users.noreply.github.com>
2021-01-18 01:12:22 -0500

    Add files via upload

---
+++ Contact_files/Contact.js
@@ -0,0 +1,7 @@
+// Created by iWeb 3.0.4 local-build-20191106
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Contact_files/ContactMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
---
+++ Photos_files/Photos.js
@@ -0,0 +1,7 @@
+// Created by iWeb 3.0.4 local-build-20191106
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Photos_files/PhotosMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
